497d5db51fd64acf47b9b7798d8ed5834c2fd167
Lars Maurath <larsmaurath@gmail.com>
2021-04-13 23:17:53 -0400

    Remove mxnet dependency

--- narya/narya/tracker/homography_estimator.py
+++ narya/narya/tracker/homography_estimator.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import mxnet as mx
+#import mxnet as mx
 import numpy as np
 import tensorflow as tf
 
