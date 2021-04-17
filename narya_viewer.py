df20fab9e64543b5ab035593ae0e0f90fcc2567c
Lars Maurath <larsmaurath@gmail.com>
2021-04-17 12:53:49 -0400

    Update narya_viewer.py

--- narya_viewer.py
+++ narya_viewer.py
@@ -1,8 +1,6 @@
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
 import pandas as pd
-from helpers import Homography, VoronoiPitch, Play, PitchImage, PitchDraw, get_table_download_link
-from pitch import FootballPitch
+from helpers import get_table_download_link
 from narya.narya.tracker.full_tracker import FootballTracker
 import cv2
 import numpy as np
@@ -19,11 +17,17 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 image = None
 
+# This is a work-around to disable the entity tracker and analyze multiple frames
+# With the same tracker
+@st.cache(allow_output_mutation=True)
+def create_players_to_remove():
+    return []
+
 @st.cache(allow_output_mutation=True)
 def create_tracker():
     tracker = FootballTracker(pretrained=True, 
                               frame_rate=23,
-                              track_buffer = 60,
+                              track_buffer = 0,
                               ctx=None)
         
     return tracker
@@ -31,80 +35,89 @@ def create_tracker():
 template = cv2.imread('narya/world_cup_template.png')
 template = cv2.resize(template, (512,512))/255.
 
-image_selection = st.selectbox('Choose image:', ['', 'Example Image', 'My own image'], 
+image_selection = st.selectbox('Choose image:', ['', 'Example Image', 'Upload Image'], 
                         format_func=lambda x: 'Choose image' if x == '' else x)
 
 if image_selection:
     if image_selection == 'Example Image':
         image = cv2.imread("atm_che_23022021_62_07_2.jpg")
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         
     else:
         
-        st.title('Upload Image or Video')
-        uploaded_file = st.file_uploader("Select Image file to open:", type=["png", "jpg", "mp4"])
-        pitch = FootballPitch()
+        st.title('Upload Image')
+        uploaded_file = st.file_uploader("Select Image file to open:", type=["png", "jpg"])
         
         if uploaded_file:
-            if uploaded_file.type == 'video/mp4':
-                play = Play(uploaded_file)
-                t = st.slider('You have uploaded a video. Choose the frame you want to process:', 0.0,60.0)
-                image = play.get_frame(t)
-                image = cv2.imread("atm_che_23022021_62_07_2.jpg")
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-            else:
                 
-                file_bytes = np.asarray(bytearray(uploaded_file.read()),dtype=np.uint8)
-                image = cv2.imdecode(file_bytes, 1)
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            file_bytes = np.asarray(bytearray(uploaded_file.read()),dtype=np.uint8)
+            image = cv2.imdecode(file_bytes, 1)
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     
     if image is not None:
     
         img_list = [image]
         
         tracker = create_tracker()
+        players_to_remove = create_players_to_remove()
         
-        trajectories = tracker(img_list,
-                               split_size = 512, 
-                               save_tracking_folder = 'narya_output/', 
-                               template = template, 
-                               skip_homo = [])
         
-        x_coords = [val[0][0] for val in trajectories.values()]
-        y_coords = [val[0][1] for val in trajectories.values()]
-    
-        x_coords = [x/320*120 for x in x_coords]
-        y_coords = [y/320*80 for y in y_coords]
-    
-        pitch = Pitch(view='full', figsize=(6.8, 10.5), orientation='horizontal')
-        fig, ax = pitch.draw()
+        try:
         
-        pitch.scatter(x_coords, y_coords, ax=ax, c='#c34c45', s=150)
-    
-        st.title('Tracking Results')
-    
-        st.write('From left to right: the original image, overlayed bounding boxes + homography and a schematic represenation',
-                                          expanded=True)
-        
-        col1, col2, col3 = st.beta_columns(3)
-        
-        with col1:
-            st.image(image, use_column_width= 'always')
-
-        with col2:
-            st.image("narya_output/test_00000.jpg", use_column_width= 'always')
-
-        with col3: 
+            trajectories = tracker(img_list,
+                                   split_size = 512, 
+                                   save_tracking_folder = 'narya_output/', 
+                                   template = template, 
+                                   skip_homo = [])
             
-            st.pyplot(fig)
-
-        review = st.selectbox('Do the results look good?:', ['', 'Yes and export', 'No and manually fix'], 
-                                format_func=lambda x: 'Do the results look good?' if x == '' else x)
+            
+            trajectories = {k: v for k, v in trajectories.items() if k not in players_to_remove}
+            
+            players_to_remove += list(trajectories.keys())
+            
+            x_coords = [val[0][0] for val in trajectories.values()]
+            y_coords = [val[0][1] for val in trajectories.values()]
+            colors = [val[0][3] for val in trajectories.values()]
+            colors = [tuple([item / 255 for item in subl]) for subl in colors]
         
-        if review:
-            if review == 'Yes and export':
-                df = pd.DataFrame({'x': x_coords, 'y': y_coords})
-                st.markdown(get_table_download_link(df[['x', 'y']]), unsafe_allow_html=True)
+            x_coords = [x/320*120 for x in x_coords]
+            y_coords = [y/320*80 for y in y_coords]
+        
+            plot = Pitch(view='full', figsize=(6.8, 10.5), orientation='horizontal')
+            fig, ax = plot.draw()
+            
+            plot.scatter(x_coords, y_coords, ax=ax, c='#c34c45', s=150)
+        
+            st.title('Tracking Results')
+        
+            st.write('From left to right: the original image, overlayed bounding boxes + homography and a schematic represenation',
+                                              expanded=True)
+            
+            col1, col2, col3 = st.beta_columns(3)
+            
+            with col1:
+                st.image(image, use_column_width= 'always')
+    
+            with col2:
+                image_processed = cv2.imread("narya_output/test_00000.jpg")
+                image_processed = cv2.cvtColor(image_processed, cv2.COLOR_BGR2RGB)
+                st.image(image_processed, use_column_width= 'always')
+    
+            with col3: 
                 
-            else:
-                st.write("I hope to soon add the functionality to annotate players and homography manually")
+                st.pyplot(fig)
+            
+            review = st.selectbox('Do the results look good?:', ['', 'Yes and export', 'No and manually fix'], 
+                                    format_func=lambda x: 'Do the results look good?' if x == '' else x)
+            
+            if review:
+                if review == 'Yes and export':
+                    df = pd.DataFrame({'x': x_coords, 'y': y_coords})
+                    st.markdown(get_table_download_link(df[['x', 'y']]), unsafe_allow_html=True)
+                    
+                else:
+                    st.write("I hope to soon add the functionality to annotate players and homography manually")
+                    
+        except:
+            
+            st.write('The model has not found enough keypoints')
